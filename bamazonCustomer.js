abe1c58e07acfeedf2f3d19a0756c94b46b3301f
JBrozinski <joseph.brozinski@gmail.com>
2020-04-19 15:45:52 -0600

    linked mysql database with javascript. running js file with node displays the contents of the database in terminal

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,60 @@
-Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
+// Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
 
-The app should then prompt users with two messages.
+// The app should then prompt users with two messages.
 
-   * The first should ask them the ID of the product they would like to buy.
-   * The second message should ask how many units of the product they would like to buy.
+//    * The first should ask them the ID of the product they would like to buy.
+//    * The second message should ask how many units of the product they would like to buy.
+
+// Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
+
+//    * If not, the app should log a phrase like `Insufficient quantity!`, and then prevent the order from going through.
+
+//    However, if your store _does_ have enough of the product, you should fulfill the customer's order.
+//    * This means updating the SQL database to reflect the remaining quantity.
+//    * Once the update goes through, show the customer the total cost of their purchase.
+
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  port: 3306,
+
+  user: "root",
+
+  password: "GoNuggets#1",
+  database: "bamazonDB",
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  queryAllItems();
+  //   start();
+});
+//item_id, product_name, department, price, stock_quantity
+function queryAllItems() {
+  connection.query("SELECT * FROM products", function (err, res) {
+    if (err) throw err;
+    for (var i = 0; i < res.length; i++) {
+      console.log(
+        res[i].item_id +
+          " | " +
+          res[i].product_name +
+          " | " +
+          res[i].department +
+          " | " +
+          res[i].price +
+          " | " +
+          res[i].stock_quantity
+      );
+    }
+    console.log("successfully quried products");
+  });
+}
+
+// function start() {
+//     inquirer
+//       .prompt({
+
+//       })
